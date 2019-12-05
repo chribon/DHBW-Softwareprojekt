bb2a56c34560ac20d7f3d207d7da25d109d1e5b8
Christian Bonfert <christian.bonfert>
2019-12-05 08:13:19 +0100

    Deleted venv

--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/christian/Library/Mobile Documents/com~apple~CloudDocs/DHBW/5. Semester/Softwareprojekt/DHBW-Softwareprojekt/backend/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
