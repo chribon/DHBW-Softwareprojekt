85ea459ed92de07ce4093b091192a5d4851c18b1
Christian Bonfert <christian.bonfert>
2019-12-12 15:52:26 +0100

    Register apps

--- backend/geolocation_portal/geolocation_portal/settings.py
+++ backend/geolocation_portal/geolocation_portal/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'geoamin',
+    'geoapi',
+    'geomodels',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
