7eb2034f9a86323f31e5b64cd28bd44105fa2667
Simon Gensler <hallo@simongensler.de>
2019-12-12 16:07:45 +0100

    fix app_name error in settings

--- backend/geolocation_portal/geolocation_portal/settings.py
+++ backend/geolocation_portal/geolocation_portal/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'geoamin',
+    # own
+    'geoadmin',
     'geoapi',
     'geomodels',
     'rest_framework',
