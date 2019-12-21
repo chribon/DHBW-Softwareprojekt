0b36a62bfee20eb67159b22d0196de1c2e989060
Stra1gther <hallo@simongensler.de>
2019-12-21 16:30:53 +0100

    Add static file handling, add customize option for javascript in the backend - static (development) files will now be served - the admin panel will now include our custom javascript for leaflet

---
+++ backend/geolocation_portal/geoadmin/static/geoadmin/leaflet_customize_mosbach.js
@@ -0,0 +1,5 @@
+/* customize leaflet in the admin panel
+ *
+ * please note, that we have to write the javascript leaflet agnostic,
+ * that is, it has to work if there are 0,1,2, ..., n leaflet-maps in the admin panel!
+*/
