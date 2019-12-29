cefe2b800cf74e54ae60d3c84097c6a4e34ca5a8
Stra1gther <hallo@simongensler.de>
2019-12-29 17:20:21 +0100

    Add fullscreen support for maps in admin app

--- backend/geolocation_portal/geoadmin/static/geoadmin/leaflet_customize_mosbach.js
+++ backend/geolocation_portal/geoadmin/static/geoadmin/leaflet_customize_mosbach.js
@@ -6,10 +6,10 @@
 */
 window.addEventListener("map:init", function (event) {
 //    window.alert("map is loaded");
-    var map = event.detail.map;
-    console.log(map);
+	var map = event.detail.map;
+	console.log(map);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3RyYWlnaHRlciIsImEiOiJjazRmc3Q1NXAwcG1sM3RvMmExbGdqbGt2In0.RwSVBnVl6hNJn_avx5DIug', {
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3RyYWlnaHRlciIsImEiOiJjazRmc3Q1NXAwcG1sM3RvMmExbGdqbGt2In0.RwSVBnVl6hNJn_avx5DIug', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -17,8 +17,13 @@ window.addEventListener("map:init", function (event) {
 		id: 'mapbox/streets-v11'
 	}).addTo(map);
 
-    map.setZoom(13);
-    var mosbach = L.latLng(49.352554, 9.145346);
-    map.panTo(mosbach);
-});
+	map.setZoom(13);
+	var mosbach = L.latLng(49.352554, 9.145346);
+	map.panTo(mosbach);
 
+	L.control.fullscreen({
+		position: 'topleft',
+		title: 'Im Vollbild anzeigen',
+		titleCancel: 'Vollbild verlassen'
+	}).addTo(map);
+});
