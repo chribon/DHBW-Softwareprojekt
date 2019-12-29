691850d9360fc9192b46b5ae456cd75b9816406e
Stra1gther <hallo@simongensler.de>
2019-12-29 13:01:41 +0100

    Make Category readonly in admin-page

--- backend/geolocation_portal/geomodels/admin.py
+++ backend/geolocation_portal/geomodels/admin.py
@@ -7,7 +7,22 @@ from .models import (
     Subcategory,
 )
 
-class CategoryAdmin(admin.ModelAdmin):
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = ['title', 'description']
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class CategoryAdmin(ReadOnlyAdmin):
     list_display = ['title']
     ordering = ['title']
     search_fields = ['title']
@@ -22,7 +37,6 @@ class SubcategoryAdmin(admin.ModelAdmin):
 
     def hauptkategorie(self, instance):
         return instance.id_category.title
-
 
 admin.site.register(Subcategory, SubcategoryAdmin)
 
