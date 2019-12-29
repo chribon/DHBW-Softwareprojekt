f4adbde508bf0211d447fe7655ac9c00b3931a30
Stra1gther <hallo@simongensler.de>
2019-12-29 12:53:59 +0100

    Remove old code

--- backend/geolocation_portal/geomodels/admin.py
+++ backend/geolocation_portal/geomodels/admin.py
@@ -5,10 +5,7 @@ from leaflet.admin import LeafletGeoAdmin, LeafletGeoAdminMixin
 from .models import (
     Category,
     Subcategory,
-    #GlassTrashEntry,
 )
-
-# Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title']
@@ -29,31 +26,3 @@ class SubcategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Subcategory, SubcategoryAdmin)
 
-
-### Specific Entries ###
-
-# class GlassTrashEntryAdmin(LeafletGeoAdmin):
-#     list_display = ['title', 'unterkategorie']
-#     ordering = ['title']
-#     search_fields = ['title']
-
-#     def unterkategorie(self, instance):
-#         return instance.id_subcategory.title
-
-# admin.site.register(GlassTrashEntry, GlassTrashEntryAdmin)
-
-# class ClothingTrashEntryAdmin(LeafletGeoAdmin):
-#     list_display = ['title', 'unterkategorie']
-#     ordering = ['title']
-#     search_fields = ['title']
-
-#     def unterkategorie(self, instance):
-#         return instance.id_subcategory.title
-
-# class BatteryTrashEntryAdmin(LeafletGeoAdmin):
-#     list_display = ['title', 'unterkategorie']
-#     ordering = ['title']
-#     search_fields = ['title']
-
-#     def unterkategorie(self, instance):
-#         return instance.id_subcategory.title
