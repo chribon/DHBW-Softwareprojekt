c5cb3a0a4ca2f9f8a17d9996508d4c437f14b92a
lamon4 <luca.amon@web.de>
2020-01-01 15:29:05 +0100

    added searchbar + functionality

--- frontend/geolocation-portal/src/app/api-data.ts
+++ frontend/geolocation-portal/src/app/api-data.ts
@@ -3,22 +3,22 @@ import { Subcategory } from './subcategory';
 import { Entry } from './entry';
 
 export const CATEGORIES: Category[] = [
-  { id: 1, title: 'kategorie1' , description:"test"},
-  { id: 2, title: 'kategorie2', description:"test" },
-  { id: 3, title: 'kategorie3', description:"test" },
-  { id: 4, title: 'kategoryie', description:"test" },
-  { id: 5, title: 'kategorie5', description:"test" },
-  { id: 6, title: 'kategorie6', description:"test" },
+  { id: 1, title: 'category1' , description:"test"},
+  { id: 2, title: 'category2', description:"test" },
+  { id: 3, title: 'category3', description:"test" },
+  { id: 4, title: 'category4', description:"test" },
+  { id: 5, title: 'category5', description:"test" },
+  { id: 6, title: 'category6', description:"test" },
 ];
 
 
 export const SUBCATEGORIES: Subcategory[] = [
-  { id: 1, cid: 1, title: 'subcategorie1.1', type:'"map' },
-  { id: 2, cid: 1, title: 'subcategorie1.2', type:'"map' },
-  { id: 3, cid: 2, title: 'subcategorie2.1', type:'map' },
-  { id: 4, cid: 2, title: 'subcategorie2.2', type:'map' },
-  { id: 5, cid: 3, title: 'subcategorie3.1', type:'map' },
-  { id: 6, cid: 3, title: 'subcategorie3.2', type:'map' },
+  { id: 1, cid: 1, title: 'subcategory1.1', type:'"map' },
+  { id: 2, cid: 1, title: 'subcategory1.2', type:'"map' },
+  { id: 3, cid: 2, title: 'subcategory2.1', type:'map' },
+  { id: 4, cid: 2, title: 'subcategory2.2', type:'map' },
+  { id: 5, cid: 3, title: 'subcategory3.1', type:'map' },
+  { id: 6, cid: 3, title: 'subcategory3.2', type:'map' },
 ];
 export const ENTRIES: Entry[] = [
   {id: 1, title: "mülleimer 1", content: [{lat:49.346881, long: 9.135313 }]},
--- frontend/geolocation-portal/src/app/menu/menu.component.ts
+++ frontend/geolocation-portal/src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
 import { Category } from '../category';
+import { Subcategory } from '../subcategory';
+import { SubcategoryService } from '../subcategory.service';
 
 
 
@@ -10,15 +12,42 @@ import { Category } from '../category';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
   categories: Category[];
-  constructor(private categoryService: CategoryService) { }
+  subcategories: Subcategory[];
+  searchCategories: Category[];
+
+  constructor(private categoryService: CategoryService,
+    private subcategoryService: SubcategoryService) { }
+
+  searchTerm: string;
 
   ngOnInit() {
     this.getCategories();
+    this.getSubcategories();
+  }
+
+  search() {
+    this.searchCategories = [];
+    for (let category of this.categories) {
+      if (category.title.includes(this.searchTerm)) {
+        this.searchCategories.push(category);
+      }
+    }
+    for (let subcategory of this.subcategories) {
+      if (subcategory.title.includes(this.searchTerm)) {
+        this.searchCategories.push(this.categories.find(category => category.id == subcategory.cid));
+      }
+    }
+    console.log(this.searchCategories);
 
   }
-  getCategories(){
-   this.categoryService.getCategories().subscribe( CATEGORIES => (this.categories = CATEGORIES));
+
+  getSubcategories(): void {
+    this.subcategoryService.getSubcategories().subscribe(Subcategory => (this.subcategories = Subcategory));
   }
- 
+  getCategories() {
+    this.categoryService.getCategories().subscribe(CATEGORIES => (this.categories = CATEGORIES));
+  }
+
 }
