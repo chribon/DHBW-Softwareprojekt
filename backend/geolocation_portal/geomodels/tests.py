8f1c4d2e8b8282292af0cdc09ac7a099a21e21c7
Stra1gther <hallo@simongensler.de>
2020-01-03 08:12:32 +0100

    Add integration tests for intial geomodels data

--- backend/geolocation_portal/geomodels/tests.py
+++ backend/geolocation_portal/geomodels/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import (
+    Category,
+    Subcategory,
+)
 
-# Create your tests here.
+class IntegrationTests(TestCase):
+    fixtures = ['geomodels/fixtures/initial_data.yaml']
+
+    def test_number_of_categories_greater_equal_six(self):
+        self.assertGreaterEqual(
+            Category.objects.count(),
+            6
+        )
+
+    def test_number_of_subcategories_greater_equal_seventeen(self):
+        self.assertGreaterEqual(
+            Subcategory.objects.count(),
+            17
+        )
+
