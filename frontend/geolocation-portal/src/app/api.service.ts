ece3fa06e7ff6dd0a834373b3488a401886c61e9
Stra1gther <hallo@simongensler.de>
2020-01-15 22:53:39 +0100

    Setup frontend for production setup

--- frontend/geolocation-portal/src/app/api.service.ts
+++ frontend/geolocation-portal/src/app/api.service.ts
@@ -5,11 +5,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError} from 'rxjs/operators';
 import { FeatureCollection } from './Models/FeatureCollection/featurecollection';
 import { Category } from './Models/category';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
+
+  baseUrl = environment.baseUrl;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,19 +21,19 @@ export class APIService {
   constructor(private http: HttpClient) { }
 
   getCategoriesFromAPI(): Observable<Category[]> {
-    return this.http.get<Category[]>("http://127.0.0.1:8000/api/categories/").pipe(
+      return this.http.get<Category[]>(this.baseUrl + "/api/categories/").pipe(
       catchError(this.handleError<Category[]>('getCategoriesFromAPI', []))
     );
   }
 
   getSubcategoriesFromAPI(): Observable<Subcategory[]> {
-    return this.http.get<Subcategory[]>("http://127.0.0.1:8000/api/subcategories/").pipe(
+    return this.http.get<Subcategory[]>(this.baseUrl + "/api/subcategories/").pipe(
       catchError(this.handleError<Subcategory[]>('getSubcategoriesFromAPI', []))
     );
   }
 
   getEntriesFromAPI(subcategoryID: number): Observable<FeatureCollection> {
-    return this.http.get<FeatureCollection>("http://127.0.0.1:8000/api/subcategories/" + subcategoryID + "/entries/").pipe(
+      return this.http.get<FeatureCollection>(this.baseUrl + "/api/subcategories/" + subcategoryID + "/entries/").pipe(
       catchError(this.handleError<FeatureCollection>('getEntriesFromAPI', null))
     );
   }
