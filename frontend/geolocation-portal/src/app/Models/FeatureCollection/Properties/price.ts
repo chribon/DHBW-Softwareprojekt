8160b60bfbe80d72b0b793efa5401dbe8f310fe8
lamon4 <luca.amon@web.de>
2020-01-18 16:16:03 +0100

    expanded link in header to logo. checking for null values in info box

--- frontend/geolocation-portal/src/app/Models/FeatureCollection/Properties/price.ts
+++ frontend/geolocation-portal/src/app/Models/FeatureCollection/Properties/price.ts
@@ -2,11 +2,10 @@ import { InfoObject } from './infoObject';
 
 export class Price extends InfoObject{
   price: string;
-  type: string;
+
 
   constructor(price: string, type:string){
     super();
-    this.type = type;
       this.price = price;
   }
 }
